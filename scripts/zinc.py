6daed1366dd19c256d318b413dfea694162ee9d1
yuanqing-wang <wangyq@wangyq.net>
2018-12-29 22:05:46 +0800

    used two tapes

--- scripts/zinc.py
+++ scripts/zinc.py
@@ -96,46 +96,46 @@ for epoch in range(1000):
         # TODO
         # one training batch
         with tf.GradientTape(persistent=True) as tape: # for descent
-            eo_f, h_f = enc_f(xs)
-            eo_b, h_b = enc_b(xs)
-            attention_weights = attention(eo_f, eo_b, h_f, h_b)
-            attention_weights = fcuk(attention_weights)
-            ys_hat = fcuk_props(attention_weights)
-            loss0 = tf.losses.mean_squared_error(ys_hat, ys)
-            if loss0_int == 0.0:
-                loss0_int = loss0
+            with tf.GradientTape(persistent=True) as subtape:
+                eo_f, h_f = enc_f(xs)
+                eo_b, h_b = enc_b(xs)
+                attention_weights = attention(eo_f, eo_b, h_f, h_b)
+                attention_weights = fcuk(attention_weights)
+                ys_hat = fcuk_props(attention_weights)
+                loss0 = tf.losses.mean_squared_error(ys_hat, ys)
+                if loss0_int == 0.0:
+                    loss0_int = loss0
 
-            dec_input = tf.expand_dims([lang_obj.ch2idx['G']] * BATCH_SZ, 1)
-            dec_hidden = decoder.initialize_hidden_state()
-            loss1 = 0
-            for t in range(xs.shape[1]):
-                ch_hat, dec_hidden = decoder(dec_input, dec_hidden, attention_weights)
-                loss1 += seq_loss(xs[:, t], ch_hat)
-                dec_input = tf.expand_dims(xs[:, t], 1)
-            if loss1_int == 0.0:
-                loss1_int = loss1
+                dec_input = tf.expand_dims([lang_obj.ch2idx['G']] * BATCH_SZ, 1)
+                dec_hidden = decoder.initialize_hidden_state()
+                loss1 = 0
+                for t in range(xs.shape[1]):
+                    ch_hat, dec_hidden = decoder(dec_input, dec_hidden, attention_weights)
+                    loss1 += seq_loss(xs[:, t], ch_hat)
+                    dec_input = tf.expand_dims(xs[:, t], 1)
+                if loss1_int == 0.0:
+                    loss1_int = loss1
 
-            with tf.GradientTape(persistent=True) as tape_1:
-                # start grad norm
-                loss0_var = enc_f.variables + enc_b.variables + attention.variables + fcuk_props.variables
-                loss1_var = decoder.variables
-                lt = w0_task * loss0 + w1_task * loss1
-                gw0 = w0_task * tf.norm(tape.gradient(loss0,
-                  loss0_var)[0].values)
-                gw1 = w1_task * tf.norm(tape.gradient(loss1,
-                  loss1_var)[0].values)
-                gw_bar = 0.5 * (gw0 + gw1)
-                l0_tilde = tf.div(loss0, loss0_int)
-                l1_tilde = tf.div(loss1, loss1_int)
-                l_tilde_bar = 0.5 * (l0_tilde + l1_tilde)
-                r0 = tf.div(l0_tilde, l_tilde_bar)
-                r1 = tf.div(l1_tilde, l_tilde_bar)
+            # start grad norm
+            loss0_var = enc_f.variables + enc_b.variables + attention.variables + fcuk_props.variables
+            loss1_var = decoder.variables
+            lt = w0_task * loss0 + w1_task * loss1
+            gw0 = w0_task * tf.norm(subtape.gradient(loss0,
+              loss0_var)[0].values)
+            gw1 = w1_task * tf.norm(subtape.gradient(loss1,
+              loss1_var)[0].values)
+            gw_bar = 0.5 * (gw0 + gw1)
+            l0_tilde = tf.div(loss0, loss0_int)
+            l1_tilde = tf.div(loss1, loss1_int)
+            l_tilde_bar = 0.5 * (l0_tilde + l1_tilde)
+            r0 = tf.div(l0_tilde, l_tilde_bar)
+            r1 = tf.div(l1_tilde, l_tilde_bar)
 
-                l_grad = tf.math.abs(gw0 - tf.stop_gradient(gw_bar * tf.math.pow(r0, alpha))) +\
-                         tf.math.abs(gw1 - tf.stop_gradient(gw_bar * tf.math.pow(r1, alpha)))
+            l_grad = tf.math.abs(gw0 - tf.stop_gradient(gw_bar * tf.math.pow(r0, alpha))) +\
+                     tf.math.abs(gw1 - tf.stop_gradient(gw_bar * tf.math.pow(r1, alpha)))
 
-        delta_l_grad_0 = tape_1.gradient(l_grad, w0_task)
-        delta_l_grad_1 = tape_1.gradient(l_grad, w1_task)
+        delta_l_grad_0 = tape.gradient(l_grad, w0_task)
+        delta_l_grad_1 = tape.gradient(l_grad, w1_task)
         optimizer.apply_gradients(zip([delta_l_grad_0], [w0_task]))
         optimizer.apply_gradients(zip([delta_l_grad_1], [w1_task]))
 
