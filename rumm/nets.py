8085d7a1a96258bf3fdb676f924286eeefc638aa
yuanqing-wang <wangyq@wangyq.net>
2019-02-28 23:15:00 -0500

    updated training scheme

--- rumm/nets.py
+++ rumm/nets.py
@@ -62,9 +62,9 @@ def lstm(units):
         return tf.keras.layers.CuDNNLSTM(units,
                                     return_sequences=True,
                                     return_state=True,
-                                    recurrent_initializer='glorot_uniform',
+                                    recurrent_initializer='glorot_normal',
                                     kernel_regularizer=tf.keras.regularizers.l1(l=0.0001),
-                                    recurrent_regularizer=tf.keras.regularizers.l1(l=0.00001),
+                                    recurrent_regularizer=tf.keras.regularizers.l1(l=0.0001),
                                     bias_regularizer=tf.keras.regularizers.l1(l=0.0001))
     else:
         return tf.keras.layers.LSTM(units,
@@ -74,10 +74,10 @@ def lstm(units):
                                recurrent_initializer='glorot_uniform')
 
 
-class OneHotDecoder(tf.keras.Model):
+class OneHotShallowDecoder(tf.keras.Model):
     """ """
     def __init__(self, vocab_size, dec_units = 32, batch_sz = 128, max_len = 64):
-        super(OneHotDecoder, self).__init__()
+        super(OneHotShallowDecoder, self).__init__()
         self.vocab_size = vocab_size
         self.dec_units = dec_units
         self.batch_sz = batch_sz
@@ -99,6 +99,198 @@ class OneHotDecoder(tf.keras.Model):
         x_ = self.fc(x)
         return x_
 
+class OneHotDecoder(tf.keras.Model):
+    def __init__(self, vocab_size, dec_units = 32, batch_sz = 128, max_len = 64):
+        super(OneHotDecoder, self).__init__()
+        self.vocab_size = vocab_size
+        self.dec_units = dec_units
+        self.batch_sz = batch_sz
+        self.gru_f = gru(self.dec_units)
+        self.gru_b = gru(self.dec_units, reverse=True)
+        self.lstm1_f = lstm(self.dec_units)
+        self.lstm1_b = lstm(self.dec_units, reverse=True)
+        self.lstm2 = lstm(self.dec_units)
+        self.lstm3 = lstm(self.dec_units)
+
+        self.max_len = max_len
+        self.fc = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(vocab_size))
+        self.fc1 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(vocab_size))
+        self.fc2 = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(vocab_size))
+        self.D = tf.keras.layers.Dense(2 * dec_units)
+        self.D1 = tf.keras.layers.Dense(2 * dec_units)
+        # self.D_bypass0 = tf.keras.layers.Dense(2 * dec_units)
+        # self.D_bypass = tf.keras.layers.Dense(vocab_size * max_len)
+
+    @tf.contrib.eager.defun
+    def _call(self, x_):
+        x = self.D(x_)
+        x = self.D1(x)
+        x = tf.expand_dims(x, 1)
+        x = tf.tile(x, [1, self.max_len, 1])
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x_f, _, _ = self.lstm1_f(x)
+        x_b, _, _ = self.lstm1_b(x)
+        x = tf.concat([x_f, x_b], -1)
+        x, _, _ = self.lstm2(x)
+        x, _, _ = self.lstm3(x)
+        x = self.fc(x)
+        # x = self.fc1(x)
+
+        # x_bypass = self.D_bypass(self.D_bypass0(x_))
+        # x = self.fc(x) + tf.reshape(x_bypass, [x_bypass.shape[0], self.max_len, self.vocab_size])
+        return x
+
+    def __call__(self, x):
+        return self._call(x)
+
+class ConvEncoder(tf.keras.Model):
+    """ Encoding sequence onto a latent space.
+
+    Attributes
+    ----------
+    conv_units : list
+        list of units of conv layers
+    pool_sizes : list
+        list of sizes of pooling layers
+    conv_kernel_sizes : list
+        list of sizes of conv kernels
+    fcs : list
+        list of fully connected layers configurations.
+            - str -> activation function
+            - int -> one layer with neuron
+            - float -> dropout
+
+    Methods
+    -------
+    build : building the model
+    switch : switch between training and test
+
+    """
+
+    def __init__(
+        self,
+        conv_units: list,
+        # pool_sizes: list,
+        conv_kernel_sizes: list,
+        fcs: list) -> None:
+
+        # bookkeeping
+        super(ConvEncoder, self).__init__()
+        self.conv_units = conv_units
+        # self.pool_sizes = pool_sizes
+        self.conv_kernel_sizes = conv_kernel_sizes
+        self.fcs = fcs
+
+        # define the workflow
+        self.workflow = []
+
+        # run build() once to construct the model
+        self.build()
+
+
+    def build(self):
+        # assert len(self.conv_units) == len(self.pool_sizes)
+        assert len(self.conv_units) == len(self.conv_kernel_sizes)
+
+        # set convolution layers
+        for idx in range(len(self.conv_units)):
+            # get the names
+            conv_name = 'C' + str(idx)
+            # pool_name = 'P' + str(idx)
+            self.workflow.append(conv_name)
+            # self.workflow.append(pool_name)
+
+            # get the configs
+            conv_unit = self.conv_units[idx]
+            conv_kernel_size = self.conv_kernel_sizes[idx]
+            # pool_size = self.pool_sizes[idx]
+
+            # set the attributes
+            # conv layers
+            setattr(
+                self,
+                conv_name,
+                tf.keras.layers.Conv1D(
+                    conv_unit, conv_kernel_size,
+                    kernel_regularizer=tf.keras.regularizers.l2(l=0.01),
+                    bias_regularizer=tf.keras.regularizers.l2(l=0.01),
+                    activation = tf.nn.tanh))
+
+            '''
+            # pool layers
+            setattr(
+                self,
+                pool_name,
+                tf.keras.layers.MaxPooling1D(pool_size)
+                )
+            '''
+
+
+        # flatten it
+        setattr(self, 'F', tf.layers.flatten)
+        self.workflow.append('F')
+
+        for idx, value in enumerate(self.fcs):
+            # str -> activation function
+            if isinstance(value, str):
+                # within our selction
+                assert value in ["tanh", "relu", "sigmoid", "leaky_relu", "elu"]
+
+                name = 'A' + str(idx)
+                if value == 'tanh':
+                    setattr(self, name, tf.tanh)
+                elif value == 'relu':
+                    setattr(self, name, tf.nn.relu)
+                elif value == 'sigmoid':
+                    setattr(self, name, tf.sigmoid)
+                elif value == 'leaky_relu':
+                    setattr(self, name, tf.nn.leaky_relu)
+                elif value == 'elu':
+                    setattr(self, name, tf.nn.elu)
+
+            # int -> fc layer
+            elif isinstance(value, int):
+                assert value >= 1
+                name = 'D' + str(idx)
+                setattr(self, name,
+                    tf.keras.layers.Dense(
+                        value,
+                        kernel_regularizer=tf.keras.regularizers.l2(0.01),
+                        bias_regularizer=tf.keras.regularizers.l2(0.01)))
+
+            # float -> dropout
+            elif isinstance(value, float):
+                assert value < 1
+                name = 'O' + str(idx)
+                setattr(self, name,
+                    tf.layers.Dropout(value))
+
+            self.workflow.append(name)
+
+
+    @tf.contrib.eager.defun
+    def _call(self, x):
+        for name in self.workflow:
+            x = getattr(self, name)(x)
+        return x
+
+
+    def __call__(self, x):
+        return self._call(x)
+
+    def switch(self, to_test = True):
+        if to_test == True:
+            for idx, name in enumerate(self.workflow):
+                if name.startswith('O'):
+                    setattr(self, name, lambda x: x)
+        else:
+            for idx, value in enumerate(self.fcs):
+                if isinstance(value, float):
+                    assert value < 1
+                    name = 'O' + str(idx)
+                    setattr(self, name,
+                       tf.layers.Dropout(value))
 
 # to be merged with OneHotDecoder
 class SimpleDecoder(tf.keras.Model):
@@ -157,11 +349,14 @@ class FullyConnectedUnits(tf.keras.Model):
                 setattr(self, 'C' + str(idx), tf.layers.Dropout(value))
                 self.dropout_list.append('C' + str(idx))
 
-    # @tf.contrib.eager.defun
-    def __call__(self, x_tensor):
+    @tf.contrib.eager.defun
+    def _call(self, x_tensor):
         for callable in self.callables:
             x_tensor = getattr(self, callable)(x_tensor)
         return x_tensor
+
+    def __call__(self, x):
+        return self._call(x)
 
     def initialize(self, x_shape):
         """
@@ -189,37 +384,32 @@ class FullyConnectedUnits(tf.keras.Model):
         return self.get_input_shape_at(0)
 
 
-class Encoder(tf.keras.Model):
+class GRUEncoder(tf.keras.Model):
     """Encoder encodes a preprocessed sequence data using GRU."""
 
-    def __init__(self, vocab_size, embedding_dim=16, enc_units=128, batch_sz=16, reverse = False):
-        super(Encoder, self).__init__()
+    def __init__(self, vocab_size, enc_units=128, batch_sz=16, reverse = False):
+        super(GRUEncoder, self).__init__()
         self.batch_sz = batch_sz
         self.enc_units = enc_units
-        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
         if reverse == False:
             self.gru_f = gru(self.enc_units)
         elif reverse == True:
             self.gru_f = gru(self.enc_units, True)
 
-    def __call__(self, x_tensor):
-        x_tensor = self.embedding(x_tensor)
-        output, state = self.gru_f(x_tensor, initial_state = tf.zeros((self.batch_sz, self.enc_units)))
-        return output, state
+        self.gru1 = gru(self.enc_units)
+        self.gru2 = gru(self.enc_units)
 
-    def initialize(self, x_shape):
-        """
+    @tf.contrib.eager.defun
+    def _call(self, xs):
+        x = tf.one_hot(xs, 36)
+        x, state0 = self.gru_f(x)
+        x, state1 = self.gru1(x)
+        x, state2 = self.gru2(x)
+        state = tf.concat([state0, state1, state2], axis=-1)
+        return x, state
 
-        Parameters
-        ----------
-        x_shape :
-
-
-        Returns
-        -------
-
-        """
-        self.__call__(tf.zeros(x_shape))
+    def __call__(self, x):
+        return self._call(x)
 
 class Decoder(tf.keras.Model):
     """ """
--- scripts/vae/zinc.py
+++ scripts/vae/zinc.py
@@ -56,14 +56,21 @@ f_handle.close()
 vocab_size = len(lang_obj.idx2ch) + 1
 
 # define models
-enc_f = nets.Encoder(vocab_size=vocab_size, batch_sz = BATCH_SZ, reverse=False,
+enc_f = nets.GRUEncoder(vocab_size=vocab_size, batch_sz = BATCH_SZ, reverse=False,
     enc_units = 512)
-enc_b = nets.Encoder(vocab_size=vocab_size, batch_sz = BATCH_SZ, reverse=True,
+enc_b = nets.GRUEncoder(vocab_size=vocab_size, batch_sz = BATCH_SZ, reverse=True,
     enc_units = 512)
-attention = nets.BidirectionalWideAttention(512)
-fcuk = nets.FullyConnectedUnits([512, 'leaky_relu', 0.25, 512, 'leaky_relu'])
-d_mean = nets.FullyConnectedUnits([512])
-d_log_var = nets.FullyConnectedUnits([512])
+conv_encoder = nets.ConvEncoder(
+    conv_units=[256, 512, 512],
+    # pool_sizes=[8, 8, 8, 8],
+    conv_kernel_sizes=[8, 12, 16],
+    fcs=[128, 0.2, 'elu',
+             512, 0.2, 'elu',
+             512])
+fcuk = nets.FullyConnectedUnits([512, 'leaky_relu', 0.25, 512])
+d_mean = nets.FullyConnectedUnits([256])
+d_log_var = nets.FullyConnectedUnits([256])
+
 fcuk_props = nets.FullyConnectedUnits([9])
 fcuk_fp = nets.FullyConnectedUnits([167, 'sigmoid'])
 decoder = nets.OneHotDecoder(vocab_size=vocab_size, dec_units = 512)
@@ -87,49 +94,14 @@ x_te = None
 y_te = None
 fp_te = None
 
-# define loss function for sequence
-def seq_loss(y, y_hat):
-    mask = 1 - np.equal(y, 0)
-    loss_ = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=y_hat) * mask
-    return tf.reduce_mean(loss_)
 
-# train it!
-# loop through the epochs
 
-# train it!
-# loop through the epochs
-'''
-w0_task = tf.Variable(1.0, dtype=tf.float32)
-w1_task = tf.Variable(1.0, dtype=tf.float32)
-w2_task = tf.Variable(1.0, dtype=tf.float32)
-w3_task = tf.Variable(1.0, dtype=tf.float32)
-'''
-optimizer = tf.train.AdamOptimizer(1e-3)
-alpha = 0.5
-anneal_step = tf.constant(10000000.0, dtype=tf.float32)
+<<<<<<< HEAD
+optimizer = tf.train.AdamOptimizer(1e-5)
+anneal_step = tf.constant(100000000.0, dtype=tf.float32)
 
-'''
-@tf.contrib.eager.defun
-def update():
-    optimizer.apply_gradients([(delta_l_grad_0, w0_task)])
-    optimizer.apply_gradients([(delta_l_grad_1, w1_task)])
-    optimizer.apply_gradients([(delta_l_grad_2, w2_task)])
-    optimizer.apply_gradients([(delta_l_grad_3, w3_task)])
 
-    w0_task.assign(tf.clip_by_value(w0_task, 0.5, 2.0))
-    w1_task.assign(tf.clip_by_value(w1_task, 0.5, 2.0))
-    w2_task.assign(tf.clip_by_value(w2_task, 0.5, 2.0))
-    w3_task.assign(tf.clip_by_value(w3_task, 0.5, 2.0))
-
-    w_total = w0_task + w1_task + w2_task + w3_task
-    w0_task.assign(w0_task * tf.div_no_nan(4.0, w_total))
-    w1_task.assign(w1_task * tf.div_no_nan(4.0, w_total))
-    w2_task.assign(w2_task * tf.div_no_nan(4.0, w_total))
-    w3_task.assign(w3_task * tf.div_no_nan(4.0, w_total))
-
-'''
-
-for epoch in range(1000):
+for epoch in range(10000):
     # loop through the batches
     for (batch, (xs, ys, fps)) in enumerate(ds):
         # TODO
@@ -143,50 +115,48 @@ for epoch in range(1000):
             # training
             eo_f, h_f = enc_f(xs)
             eo_b, h_b = enc_b(xs)
-
-            bypass_xs_f = tf.layers.flatten(bypass_v_f(eo_f))
-
-            attention_weights = attention(eo_f, eo_b, h_f, h_b)
-            x = fcuk(attention_weights)
-            x = tf.concat([x, bypass_xs_f], axis=-1)
-
+            x_attention = tf.concat([h_f, h_b], axis=-1)
+            x_attention = fcuk(x_attention)
+            x_conv = conv_encoder(tf.one_hot(xs, 33))
+            x = tf.concat([x_attention, x_conv], axis=-1)
             mean = d_mean(x)
             log_var = d_log_var(x)
-            z = tf.clip_by_norm(tf.random_normal(mean.shape), 1e5) * tf.exp(log_var * .5) + mean
-
+            z_noise = tf.clip_by_norm(tf.random_normal(mean.shape), 1e5) * tf.exp(log_var * .5)
+            z = z_noise + mean
             ys_hat = fcuk_props(mean)
             fp_hat = fcuk_fp(mean)
-
-            loss0 = tf.clip_by_value(tf.losses.mean_squared_error(ys, ys_hat),
-                                     0.0, 1e5)
-            loss1 = tf.clip_by_value(tf.losses.mean_squared_error(fps, fp_hat),
-                                     0.0, 1e5)
-
-            xs_bar = decoder(z) + simple_decoder(z)
-
+            loss0 = tf.clip_by_value(tf.losses.mean_squared_error(ys, ys_hat), 0.0, 1e5)
+            loss1 = tf.clip_by_value(tf.losses.log_loss(fps, fp_hat), 0.0, 1e5)
+            xs_bar = decoder(z)
             loss2 = tf.clip_by_value(
-                    tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels = xs, logits = xs_bar)), 0.0, 1e5)
-            loss3 = tf.clip_by_value(kl_anneal * tf.reduce_mean(-0.5
-              * tf.reduce_sum(1 + log_var - tf.square(mean) - tf.exp(log_var), 1)),
-                                     1.0, 1e5)
+                    tf.reduce_mean(
+                        tf.nn.softmax_cross_entropy_with_logits_v2(labels = tf.one_hot(xs, 33), logits = xs_bar)), 0.0, 1e20)
+            loss3 = tf.clip_by_value(kl_anneal * tf.reduce_mean(-0.5 * tf.reduce_mean(1 + log_var - tf.square(mean) - tf.exp(log_var), axis=[1])), 1.0, 1e5)
 
             lt = loss0 + loss1 + loss2 + loss3
-            
-            variables = enc_f.variables + enc_b.variables\
-                  + attention.variables + fcuk_props.variables\
-                  + fcuk_fp.variables + d_mean.variables + d_log_var.variables\
-                  + decoder.variables + bypass_v_f.variables\
-                  + simple_decoder.variables
 
-            if (batch % 1000 == 0):
-                fcuk.save_weights('./fcuk.h5')
-                enc_f.save_weights('./enc_f.h5')
-                enc_b.save_weights('./enc_b.h5')
-                attention.save_weights('./attention_weights.h5')
-                fcuk_props.save_weights('./fcuk_props.h5')
-                fcuk_fp.save_weights('./fcuk_fp.h5')
-                d_mean.save_weights('./d_mean.h5')
-                d_log_var.save_weights('./d_log_var.h5')
-                decoder.save_weights('./decoder.h5')
-                bypass_v_f.save_weights('./bypass_v_f.h5')
-                simple_decoder.save_weights('./simple_decoder.h5')
+        # start grad norm
+        variables = conv_encoder.variables +\
+             d_mean.variables + decoder.variables + d_log_var.variables +\
+               enc_f.variables + enc_b.variables + fcuk.variables + attention.variables +\
+                fcuk_props.variables + fcuk_fp.variables
+
+        gradients = tape.gradient(lt, variables)
+
+        optimizer.apply_gradients(zip(gradients, variables), tf.train.get_or_create_global_step())
+
+        if batch % 100 == 0:
+            print(loss0.numpy(), loss1.numpy(), loss2.numpy(), loss3.numpy())
+
+        if (batch % 1000 == 0) and ( np.isnan(lt.numpy()) == False):
+            fcuk.save_weights('./fcuk.h5')
+            enc_f.save_weights('./enc_f.h5')
+            enc_b.save_weights('./enc_b.h5')
+            attention.save_weights('./attention_weights.h5')
+            fcuk_props.save_weights('./fcuk_props.h5')
+            fcuk_fp.save_weights('./fcuk_fp.h5')
+            d_mean.save_weights('./d_mean.h5')
+            d_log_var.save_weights('./d_log_var.h5')
+            decoder.save_weights('./decoder.h5')
+            bypass_v_f.save_weights('./bypass_v_f.h5')
+            simple_decoder.save_weights('./simple_decoder.h5')
